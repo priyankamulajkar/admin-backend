c63c2c80de949e9a290f5e0f871abe3b8fc23c2e
Priyanka Kashinath Mulajkar <pmulajkar@gmail.com>
2026-06-20 23:13:36 +0530

    Menu Category must be added by res-owner

--- src/routes/menuCategory.routes.ts
+++ src/routes/menuCategory.routes.ts
@@ -2,13 +2,15 @@ import express from 'express';
 import MenuCategoryController  from '../controllers/MenuCategoryController';
 import validateMiddleware from '../middleware/validateMiddleware';
 import menuCategoryValidatorSchema from '../validators/menuCategoryValidators';
+import authMiddleware from '../middleware/authMiddleware'
+import roleMiddleware from '../middleware/roleMiddleware';
 
 const router = express.Router();
 
 const menuCategoryController = new MenuCategoryController();
 
-router.post('/create', validateMiddleware.validate(menuCategoryValidatorSchema) , menuCategoryController.createMenuCategory);
-router.post('/create/bulk', menuCategoryController.createBulkMenuCategory)
+router.post('/create', authMiddleware, roleMiddleware("RES_OWNER"), validateMiddleware.validate(menuCategoryValidatorSchema) , menuCategoryController.createMenuCategory);
+router.post('/create/bulk', authMiddleware, roleMiddleware("RES_OWNER"), menuCategoryController.createBulkMenuCategory)
 router.put('/:id', validateMiddleware.validate(menuCategoryValidatorSchema) ,menuCategoryController.updateMenuCategory);
 router.delete('/delete/bulk', menuCategoryController.deleteBulkMenuCategory)
 router.delete('/delete/:id', menuCategoryController.deleteMenuCategory);
